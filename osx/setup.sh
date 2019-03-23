b25b7d8382bee1c9b184da763091027ae0ef0a4e
Samuel Cormier-Iijima <samuel@cormier-iijima.com>
2019-03-22 22:17:38 -0400

    Disable some keyboard shortcuts

--- osx/setup.sh
+++ osx/setup.sh
@@ -1,3 +1,18 @@
+# Disable automatic capitalization as it’s annoying when typing code
+defaults write NSGlobalDomain NSAutomaticCapitalizationEnabled -bool false
+
+# Disable smart dashes as they’re annoying when typing code
+defaults write NSGlobalDomain NSAutomaticDashSubstitutionEnabled -bool false
+
+# Disable automatic period substitution as it’s annoying when typing code
+defaults write NSGlobalDomain NSAutomaticPeriodSubstitutionEnabled -bool false
+
+# Disable smart quotes as they’re annoying when typing code
+defaults write NSGlobalDomain NSAutomaticQuoteSubstitutionEnabled -bool false
+
+# Disable auto-correct
+defaults write NSGlobalDomain NSAutomaticSpellingCorrectionEnabled -bool false
+
 printf "System - Avoid creating .DS_Store files on network volumes\n"
 defaults write com.apple.dashboard mcx-disabled -boolean TRUE
 
